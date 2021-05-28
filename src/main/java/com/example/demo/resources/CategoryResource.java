84399d09e9b3b5077c27dfeaf536639eceba3ac5
Ribeiro Filho <ribeiro.pdf@hotmail.com>
2021-05-28 17:33:41 -0300

    Test Autor

--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController @RequestMapping(value="/categories")
+import com.example.demo.domain.Autor;
+
+@RestController @RequestMapping(value="/autors")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String test() {
+	public List<Autor> test() {
 		
-		return "Working";
+		Autor a1 = new Autor(1, "Stephen King");
+		Autor a2 = new Autor(2, "George R. R. Martin");
+		
+		List<Autor> list = new ArrayList<> ();
+		list.add(a1);
+		list.add(a2);
+		
+		return list;
 		
 	}
+	
 }
